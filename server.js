c629c54a2f6a615f8e42f2d80f8c15dfdfecab3d
Clay James <clayw.james@gmail.com>
2016-04-24 17:06:54 -0400

    Added landing page

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-  res.sendFile("./public/main.html");
+  res.sendFile("./public/index.html");
+});
+
+app.get("/singleplayer", function(req, res){
+  res.sendFile(__dirname + "/public/main.html");
+});
+
+app.get("/twoplayer", function(req, res){
+  res.sendFile(__dirname + "/public/main.html");
+});
+
+app.get("/twoplayer_online", function(req, res){
+  res.sendFile(__dirname + "/public/main.html");
 });
 
 app.listen(3000);
