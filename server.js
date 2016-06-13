a69de34c71a066748649125a6f448de49c24a810
claywjames <clayw.james@gmail.com>
2016-06-12 20:55:00 -0400

    updated ball speed and fixed winning process on server

--- server.js
+++ server.js
@@ -193,6 +193,7 @@ setInterval(function(){
         io.to(game.client.id).emit("score",{player: "p1"});
         butterflyEffect(game.ball, game.ball.pastStates, framesBehind);
         if(game.p1Score == 10){
+          gameServer.deleteGame(game);
           io.to(game.host.id).emit("win", {player: "p1"});
           io.to(game.client.id).emit("win", {player: "p1"});
         }
@@ -202,6 +203,7 @@ setInterval(function(){
         io.to(game.client.id).emit("score",{player: "p2"});
         butterflyEffect(game.ball, game.ball.pastStates, framesBehind);
         if(game.p2Score == 10){
+          gameServer.deleteGame(game);
           io.to(game.host.id).emit("win", {player: "p2"});
           io.to(game.client.id).emit("win", {player: "p2"});
         }
